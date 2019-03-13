71feb5442be1ff374eecd72b71372312cfec2f65
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-13 20:30:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,11 +8,10 @@ attr_accessor :bank_account, :happiness, :hygiene
 attr_reader :name
 
 def initialize(name)
- @name = name
- @bank_account = 25
- @happiness = 8
- @hygiene = 8
-
+  @name = name
+  @bank_account = 25
+  @happiness = 8
+  @hygiene = 8
 end
 
 def happiness(new_happiness)
